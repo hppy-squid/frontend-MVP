e21eb09c3157d629f38c1d0c1320ff40097b84d4
Alice Fearn <alice.fearn@hotmail.com>
2025-03-14 15:26:54 +0100

    fixade form och la till filet funktion

--- js/form.mjs
+++ js/form.mjs
@@ -10,7 +10,7 @@ form.addEventListener("submit", function(e) {
     const firstName = document.getElementById("firstName").value;
     const lastName = document.getElementById("lastName").value;
     const email = document.getElementById("email").value;
-    const password = document.getElementById("password").value;
+    const password = document.getElementById("registerPassword").value;
 
     const userData = {
         firstName: firstName,
--- js/produkter.mjs
+++ js/produkter.mjs
@@ -1,4 +1,5 @@
 const productGrid = document.getElementById("product-grid")
+let allProducts = []; 
 if (productGrid) {
     console.log("product-grid hittades");
 }
@@ -17,15 +18,16 @@ fetch("http://localhost:8080/api/v1/allProduct", {
 })
 .then(data => {
     console.log("Produkter hämtade: ", data);
+    allProducts = data;
     displayProducts(data);
-
+    displayProducts(allProducts);
 })
 .catch(error => {
     console.error("Kunde ej hämta produkter")
 });
 
 function displayProducts(products) {
-    
+    productGrid.innerHTML = "";
 
     // addToCartBtn ej klar, här ska produkt skickas till personens cart --------------------------------
     products.forEach(product => {
@@ -92,6 +94,30 @@ function filterProducts(roastCategory, countryCategory) {
         }
     });
 }
+
+document.addEventListener("DOMContentLoaded", () => {
+    const searchInput = document.querySelector(".filters input[type='text']");
+
+    searchInput.addEventListener("input", () => {
+        const searchTerm = searchInput.value.toLowerCase();
+        const filteredProducts = allProducts.filter(product =>
+            product.name.toLowerCase().includes(searchTerm)
+        );
+        displayProducts(filteredProducts);
+    });
+
+});
+
+document.addEventListener("DOMContentLoaded", () => {
+    document.querySelector(".filters button:nth-child(3)").addEventListener("click", () => {
+        displayProducts([...allProducts].sort((a, b) => a.price - b.price));
+    });
+
+    document.querySelector(".filters button:nth-child(4)").addEventListener("click", () => {
+        displayProducts([...allProducts].sort((a, b) => b.price - a.price));
+    });
+
+});
 
 function goToProduct(name, price, image, roastLevel, originCountry, description) {
     const url = `detalj.html?name=${encodeURIComponent(name)}&price=${encodeURIComponent(price)}&image=${encodeURIComponent(image)}&roastLevel=${encodeURIComponent(roastLevel)}&originCountry=${encodeURIComponent(originCountry)}&description=${encodeURIComponent(description)}`;
